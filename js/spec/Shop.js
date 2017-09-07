a541a7ac910cbab18bfb8dfec83be2e455242f2d
paulmillen <pwmillen@gmail.com>
2017-09-07 14:40:48 +0100

    added brie and sulfuras tests

--- js/spec/Shop.js
+++ js/spec/Shop.js
@@ -9,27 +9,67 @@ describe('Shop', () => {
 
   describe('updateQuality', () => {
 
-    const sellIn = 1
-    const quality = 50
-    const item = new Item("common item", sellIn, quality)
-    const gildedRose = new Shop([item]);
+    describe('with common items', () => {
 
-    it('lowers a common item value by 1', () => {
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).toEqual(quality - 1)
-    });
+      const sellIn = 1
+      const quality = 50
+      const item = new Item("common item", sellIn, quality)
+      const gildedRose = new Shop([item]);
 
-    it('reduces quality twice as much when sellIn is at 0', () => {
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).toEqual(quality - 3)
-    });
-
-    it('does not lower a common items quality below 0', () => {
-      while (gildedRose.items[0].quality > 0) {
+      it('lowers its value by 1', () => {
         gildedRose.updateQuality();
-      };
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).toEqual(0)
+        expect(gildedRose.items[0].quality).toEqual(quality - 1)
+      });
+
+      it('quality degrades twice as much when sellIn is at 0', () => {
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).toEqual(quality - 3)
+      });
+
+      it('does not lower its quality below 0', () => {
+        while (gildedRose.items[0].quality > 0) {
+          gildedRose.updateQuality();
+        };
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).toEqual(0)
+      });
+    });
+
+    describe('Aged Brie', () => {
+
+      const sellIn = 0
+      const quality = 49
+      const agedBrie = new Item("Aged Brie", sellIn, quality)
+      const gildedRose = new Shop([agedBrie]);
+
+      it('increases its value by 1 when it gets older', () => {
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).toEqual(quality + 1)
+      });
+
+      it('cannot have a quality higher than 50', () => {
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).toEqual(quality + 1)
+      });
+    });
+
+    describe('Sulfuras', () => {
+
+      const sellIn = 1
+      const quality = 1
+      const sulfuras = new Item("Sulfuras, Hand of Ragnaros", sellIn, quality)
+      const gildedRose = new Shop([sulfuras]);
+
+      it('quality unchanged by updateQuality', () => {
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).toEqual(quality)
+      });
+
+      it('sellIn unchanged by updateQuality', () => {
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).toEqual(sellIn)
+      });
     });
   });
 
